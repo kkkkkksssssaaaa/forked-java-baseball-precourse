fa38ef25077bd517882acea68696d93b839a41e6
kkkkkksssssaaaa <kofc312@naver.com>
2022-04-16 23:27:22 +0900

    feat: add init player user when application start

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,21 @@
 package baseball;
 
+import baseball.number.Numbers;
 import baseball.user.ComputerUser;
+import baseball.user.PlayerUser;
+import baseball.util.Integers;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         ComputerUser computer = ComputerUser.init();
+
+        String consoleInput = Console.readLine();
+
+        Numbers playerNumbers = Numbers.of(
+                Integers.toIntArray(consoleInput));
+
+        PlayerUser player = PlayerUser.init(playerNumbers);
     }
 }
