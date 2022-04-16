cd1f687f9158acd22f4ae668b8373b323966a417
kkkkkksssssaaaa <kofc312@naver.com>
2022-04-17 00:50:27 +0900

    feat(Game): add instance methods

--- src/main/java/baseball/game/Game.java
+++ src/main/java/baseball/game/Game.java
@@ -1,5 +1,6 @@
 package baseball.game;
 
+import baseball.number.Numbers;
 import baseball.user.ComputerUser;
 import baseball.user.PlayerUser;
 import camp.nextstep.edu.missionutils.Console;
@@ -7,7 +8,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class Game {
 
     private final ComputerUser computerUser;
-    private final PlayerUser playerUser;
+    private PlayerUser playerUser;
 
     private Game(String consoleInput) {
         this.computerUser = ComputerUser.init();
@@ -16,6 +17,30 @@ public class Game {
 
     public static Game ofConsoleInput() {
         return new Game(Console.readLine());
+    }
+
+    public void run() {
+        // reInitializePlayer();
+    }
+
+    public ComputerUser computer() {
+        return this.computerUser;
+    }
+
+    public PlayerUser player() {
+        return this.playerUser;
+    }
+
+    public Numbers computerNumbers() {
+        return this.computerUser.numbers();
+    }
+
+    public Numbers playerNumbers() {
+        return this.playerUser.numbers();
+    }
+
+    private void reInitializePlayer() {
+        this.playerUser = PlayerUser.init(Console.readLine());
     }
 
 }
