dd10722b17eabf456c1c0cafbd5543445dbc958e
kkkkkksssssaaaa <kofc312@naver.com>
2022-09-30 23:12:27 +0900

    feat: 숫자 래핑 객체 추가

---
+++ src/main/java/baseball/number/Number.java
@@ -0,0 +1,69 @@
+package baseball.number;
+
+import baseball.util.CheckNull;
+
+public class Number {
+
+    private static final int MIN = 1;
+    private static final int MAX = 9;
+    private final int primitiveInteger;
+
+    private Number(int primitiveInteger) {
+        verifyInRange(primitiveInteger);
+
+        this.primitiveInteger = primitiveInteger;
+    }
+
+    public static Number of(int primitiveInteger) {
+        return new Number(primitiveInteger);
+    }
+
+    public static int min() {
+        return MIN;
+    }
+
+    public static int max() {
+        return MAX;
+    }
+
+    public int getPrimitive() {
+        return this.primitiveInteger;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        CheckNull.one(object);
+
+        if (object.getClass().equals(Integer.class)) {
+            return object.equals(this.getPrimitive());
+        }
+
+        if (object.getClass().equals(Number.class)) {
+            return this.getPrimitive() == ((Number) object).getPrimitive();
+        }
+
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.getPrimitive();
+    }
+
+    private void verifyInRange(int primitiveInteger) {
+        verifyGreaterThenMinimum(primitiveInteger);
+        verifyLessThenMaximum(primitiveInteger);
+    }
+
+    private void verifyGreaterThenMinimum(int primitiveInteger) {
+        if (primitiveInteger < min()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void verifyLessThenMaximum(int primitiveInteger) {
+        if (primitiveInteger > max()) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
