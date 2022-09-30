f51fcf6fd2c58b8e6d6a9639c8bd8307434a2479
kkkkkksssssaaaa <kofc312@naver.com>
2022-09-30 23:12:52 +0900

    test(Number): 숫자 래핑 객체의 테스트 클래스 추가

---
+++ src/test/java/baseball/number/NumberTest.java
@@ -0,0 +1,44 @@
+package baseball.number;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("NumberTest")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class NumberTest {
+
+    @Nested
+    @DisplayName("initialize")
+    class InitializeTest {
+
+        @Test
+        void 정수_리터럴로_초기화할_수_있다() {
+            assertDoesNotThrow(() -> Number.of(1));
+        }
+
+        @Test
+        void 최소값으로_초기화할_수_있다() {
+            assertDoesNotThrow(() -> Number.of(Number.min()));
+        }
+
+        @Test
+        void 최대값으로_초기화할_수_있다() {
+            assertDoesNotThrow(() -> Number.of(Number.max()));
+        }
+
+        @Test
+        void 최소값_이하로_초기화하면_IllegalArgumentException을_던진다() {
+            assertThrows(IllegalArgumentException.class, () -> Number.of(Number.min() - 1));
+        }
+
+        @Test
+        void 최대값_이상으로_초기화하면_IllegalArgumentException을_던진다() {
+            assertThrows(IllegalArgumentException.class, () -> Number.of(Number.max() + 1));
+        }
+    }
+}
